a44dfe07b97fb26dab0df4a5bba8661eb0baf5ba
bori0409 <44772249+bori0409@users.noreply.github.com>
2019-09-09 13:42:42 +0300

    FINAL
    WORKING

--- EchoServer/Client/Program.cs
+++ EchoServer/Client/Program.cs
@@ -21,30 +21,31 @@ namespace EchoServer
             {
                 try
                 {
-                    // Create 3-way Handshake 
+                   
 
                     // Step no : 1
                     //TCP establish connection via its socket through request to server
 
                     // Step no: 3 
                     // when TCP connection is established then client send (bytes of data) to server
-                    // 1) we can use "localhost" , it mean our own local machine IP
-                    // 2) "127.0.0.1" loopback IP ( when Client and Server both used in a same machine)
-                    // 3) Hostname ( server machine IP name --> 192.168.1.33)- Client and server are on the different machine 
-                    // In client socket you should refer the server hostname in terms of IP address for this particular machine
+                    //  we can use "localhost" , it mean our own local machine IP
+                    // "127.0.0.1" loopback IP ( when Client and Server both used in a same machine)
+                    //  Hostname ( server machine IP name --> 192.168.1.33)- Client and server are on the different machine 
+                    // In client socket we should refer the server hostname in terms of IP address for this particular machine
                     using (_clientSocket = new TcpClient("127.0.0.1", 7777))
                     {
                         using (_nstream = _clientSocket.GetStream())
                         {
                             // Data will be flushed from the buffer to the stream after each write operation
                             _sWriter = new StreamWriter(_nstream) { AutoFlush = true };
-                            Console.WriteLine("Client ready to send bytes of data to server...");
-                            Console.WriteLine("Kindly write your message here ");
+                            Console.WriteLine("Client ready to...");
+                            Console.WriteLine("Write  message here ");
                             string clientMsg = Console.ReadLine();
-                            // client ready to send (bytes of data) which has collected through user input
+                            // client ready to send
+                            // which has collected through user input
                             _sWriter.WriteLine(clientMsg);
 
-                            // Step no: 6 ........................................
+                           
                             // Client recieved the (modified server Message) sent back by server to client 
                             // perform write operation 
                             _sReader = new StreamReader(_nstream);
