4040e80bb91fc97ba8c3e36afa7f1099cbc3d9d2
bori0409 <44772249+bori0409@users.noreply.github.com>
2019-09-09 13:39:00 +0300

    Working Properly
    Now its working..
    We have the client and the server working and reciving and sending OK

--- EchoServer/EchoServer/Server.cs
+++ EchoServer/EchoServer/Server.cs
@@ -8,86 +8,72 @@ namespace EchoServer
 {
     class Server
     {
-        // public static readonly int Port = 7777;
+        public static readonly int Port = 7777; //we are just choosing it cuz it it some by defalt or smt
 
-        /* public Server()
-         {
-             
-         }*/
-        /* public static void Start()
+        static void Main(string[] args)
            {
-               IPAddress localAddress = IPAddress.Loopback;
-               TcpListener serverSocket = new TcpListener(localAddress, Port);
-               serverSocket.Start();
-               Console.WriteLine("The server strated on " + localAddress + "port"+ Port);
-               while (true)
-               {
-                   try
-                   {
-                       TcpClient clientConnection = serverSocket.AcceptTcpClient();
-                       Task.Run(() => DoIt(clientConnection));
-                   }
-                   catch (SocketException)
-                   {
-                       Console.WriteLine("Socket exeption : Will continue working");
-                       
-                   }
-               }
-           }
-   
-           private static void DoIt(TcpClient clientConnection)
-           {
-               Console.WriteLine("Incoming client " + clientConnection.Client);
-               NetworkStream stream = clientConnection.GetStream();
-               StreamReader reader = new StreamReader(stream);
-               StreamWriter writer = new StreamWriter(stream);
-               while (true)
-               {
-                   string request = reader.ReadLine();
-                   if (string.IsNullOrEmpty(request))
-                   {
-                       break;
-   
-                   }
-                   Console.WriteLine("Request:" + request);
-                   {
-                       string response = request;
-                       writer.WriteLine(response);
-                       writer.Flush();
-                   }
-                   clientConnection.Close();
-                   Console.WriteLine("Socket closed");
-               } */
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("raboti li");
 
 
-        /* private static void DoIt(TcpListener clientConnection)
-         {
-             Console.WriteLine("Incoming client " + clientConnection.Client);
-             NetworkStream stream = clientConnection.GetStream();
-             StreamReader reader = new StreamReader(stream);
-             StreamWriter writer = new StreamWriter(stream);
-             while (true)
-             {
-                 string request = reader.ReadLine();
-                 if (string.IsNullOrEmpty(request))
-                 {
-                     break;
- 
-                 }
-                 Console.WriteLine("Request:" + request);
-                 {
-                     string response = request;
-                     writer.WriteLine(response);
-                     writer.Flush();
-                 }
-                 clientConnection.Close();
-                 Console.WriteLine("Socket closed");
+
+            //public static readonly int Port = 7777;
+
+
+            IPAddress localAddress = IPAddress.Loopback; 
+            TcpListener serverSocket = new TcpListener(localAddress, Port); // we are saying we are opening the TCPListener stating the IP and the Port as Parameters 
+            serverSocket.Start();// we are starting the Listener, turning it on hahah
+            Console.WriteLine("The server strated on " + localAddress + "port" + Port); // Just to display if it worked 
+            while (true)
+            {
+                try
+                {
+                    TcpClient clientConnection = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("Client Connected");
+                    Task.Run(() => DoIt(clientConnection));
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Socket exeption : Will continue working");
+
+                }
             }
- 
-             
-         }
-         
-     }*/
+
+        }
+
+        private static void DoIt(TcpClient clientConnection)
+        {
+            Console.WriteLine("Incoming client " + clientConnection.Client);
+            NetworkStream stream = clientConnection.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+            // Auto flush 
+            while (true)
+            {
+                string request = reader.ReadLine();
+                Console.WriteLine("Recive request" + request);
+                if (string.IsNullOrEmpty(request))
+                {
+                    break;
+
+                }
+
+                Console.WriteLine("Request:" + request);
+                {
+                    string response = request;
+                    writer.WriteLine(response);
+                    writer.Flush();
+                }
+
+
+            }
+            clientConnection.Close();
+            Console.WriteLine("Socket closed");
+            Console.ReadKey();
+
+
+        } 
 
     }
 }
